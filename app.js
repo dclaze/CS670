26ce2efa15185c9e503e6ee45476145102a84f04
Doug Colaizzo <dcolaizzo@marathondata.com>
2014-05-06 19:55:03 -0400

    Added svg circle repeater

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 angular.module('CS670', []);
 
-angular.module('ngp').controller('Main', ['$scope',
+angular.module('CS670').controller('Main', ['$scope',
     function($scope) {
-        alert("YO");
+            $scope.map = {
+                height: 500,
+                width: 500
+            };
+        $scope.addObstacles = function() {
+
+            $scope.circles = [{
+                x: "10",
+                y: "20",
+                r: "5"
+            }, {
+                x: "40",
+                y: "50",
+                r: "3"
+            }];
+        }
     }
 ]);
